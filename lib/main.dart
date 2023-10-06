cdf117e359c72dc41da65f0f52e3d5a66a6dbb7e
ZeeshanAhmad786 <zeeshiranazeeshiranagmail.com>
2023-10-06 16:15:53 +0500

    LAST21

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        // useMaterial3: true,
       ),
       home:OnBoardScreen(),
     );
