ee5974206310f2f6786630517c1d8d7c068ac6c6
Adil Ramzan <138529549+adilramzan764@users.noreply.github.com>
2023-10-25 16:35:07 +0500

    Client Changes

--- lib/View/BottomBarScreens/Profile/Profile_MoreButton.dart
+++ lib/View/BottomBarScreens/Profile/Profile_MoreButton.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:socialmediaapp/View/BottomBarScreens/Home/SharePost.dart';
 
 import '../../../Controllers/OtherUser_MoreButtonContoller.dart';
 import '../../ProfileMenuButton_Screens/Draft.dart';
@@ -14,24 +15,18 @@ class Profile_MoreButton extends StatelessWidget {
   final bool otherUserProfile; // Add this line
 
   final List<String> iconsforuserprofie = [
-    'assets/archive.svg',
-    'assets/save_blue.svg',
-    'assets/draft.svg',
-    'assets/block.svg',
-    'assets/editProfile.svg',
-    'assets/copy.svg',
     'assets/share.svg',
+    'assets/draft.svg',
+    'assets/save_blue.svg',
+    'assets/settings.svg',
     // Add more items as needed
   ];
 
   final List<String> textsforuserprofile = [
-    'Archive',
-    'Saved',
-    'Draft',
-    'Block',
-    'Edit Profile',
-    'Copy profile URL',
     'Share this Profile',
+    'Draft',
+    'Saved',
+    'Settings and Privacy',
     // Add more items as needed
   ];
 
@@ -74,7 +69,7 @@ class Profile_MoreButton extends StatelessWidget {
   Widget ForUserProfile(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).size.height *
-          0.45, // Adjust the height as needed
+          0.3, // Adjust the height as needed
       width: double.infinity,
       decoration: BoxDecoration(
         color: Colors.white,
@@ -100,31 +95,32 @@ class Profile_MoreButton extends StatelessWidget {
                   child: InkWell(
                     onTap: () {
 
-                      // if (textsforuserprofile[index] == 'Saved') {
-                      //   Navigator.push(
-                      //     context,
-                      //     MaterialPageRoute(
-                      //         builder: (context) => Saved_Posts()),
-                      //   );
-                      // }
-                      // if (textsforuserprofile[index] == 'Draft') {
-                      //   Navigator.push(
-                      //     context,
-                      //     MaterialPageRoute(builder: (context) => Drafts()),
-                      //   );
-                      // }
-                      if (textsforuserprofile[index] == 'Edit Profile') {
+                      if (textsforuserprofile[index] == 'Saved') {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => ProfileEdit()),
+                          MaterialPageRoute(
+                              builder: (context) => Saved_Posts()),
                         );
                       }
-                      if (textsforuserprofile[index] == 'Block') {
+                      if (textsforuserprofile[index] == 'Draft') {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => BlockSetting()),
+                          MaterialPageRoute(builder: (context) => Drafts()),
                         );
                       }
+                      if (textsforuserprofile[index] == 'Share this Profile')
+
+                        showModalBottomSheet(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(25),
+                                topLeft: Radius.circular(
+                                    25)), // Set circular border radius here
+                          ),
+                          context: context,
+                          builder: (BuildContext context) => MyBottomSheet(),
+                        );
+
                     },
                     child: Container(
                       height: 30,
